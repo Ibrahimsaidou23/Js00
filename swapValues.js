72d5c073a630b39e60b688f0a53c66781e220234
Ibrahimsaidou23 <124426759+Ibrahimsaidou23@users.noreply.github.com>
2023-02-14 00:14:30 +0100

    swapValues.js

--- swapValues.js
+++ swapValues.js
@@ -1,5 +1,5 @@
 let a=10;
 let b=20;
-[b,a],[a,b];
-console.log("New value of a:",b);
-console.log("New value of b:",a);
+[a,b]=[b,a];
+console.log("New value of a:",a);
+console.log("New value of b:",b);
